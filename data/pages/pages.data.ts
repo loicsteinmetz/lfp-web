0944ad569fd7c5fbf503feb16333a4b57d0b9c0b
Loïc STEINMETZ <loic.steinmetz@pleiade.app>
2022-10-15 17:22:12 +0200

    Handles pages data

--- data/pages/pages.data.ts
+++ data/pages/pages.data.ts
@@ -1,5 +1,38 @@
+import axios from 'axios';
+import {mapMetadata} from '../_lfp/_lfp.data';
+
+const PAGES_ROOT = process.env.API_ROOT + '/pages'
+
 export const mapPage = (d: any): Page => ({
   id: d.id,
+  title: d.attributes.title,
+  slug: d.attributes.slug,
+  body: d.attributes.body,
   createdAt: new Date(d.attributes.createdAt),
   updatedAt: new Date(d.attributes.updatedAt),
+  publishedAt: new Date(d.attributes.publishedAt),
 })
+
+export const getPages = async (populate?: PopulatedPageOption): Promise<WithMetadata<Page[]>> => {
+  const result = (await axios.get(
+    PAGES_ROOT,
+    {
+      params: {
+        populate
+      }
+    })).data;
+  return {
+    meta: mapMetadata(result.meta),
+    data: result.data.map(mapPage),
+  }
+}
+
+export const getPage = async (id: number, populate?: PopulatedPageOption): Promise<Page> => {
+  return mapPage((await axios.get(
+    `${PAGES_ROOT}/${id}`,
+    {
+      params: {
+        populate
+      }
+    })).data.data);
+}
--- types/pages.d.ts
+++ types/pages.d.ts
@@ -1,5 +1,11 @@
 type Page = {
   id: number,
+  title: string,
+  slug: string,
+  body: string,
   createdAt: Date,
   updatedAt: Date,
+  publishedAt: Date,
 }
+
+type PopulatedPageOption = undefined;
