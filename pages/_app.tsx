e9c7fc910701b296a78bc2236032f84bf5c76744
Loïc STEINMETZ <loic.steinmetz@pleiade.app>
2022-12-16 16:50:51 +0100

    Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
 import '../styles/reset.css'
 import '../styles/fonts.css'
 import type {AppProps} from 'next/app'
+import {Analytics} from '@vercel/analytics/react';
 
 function MyApp({Component, pageProps}: AppProps) {
   return (
-    <Component {...pageProps} />
+    <>
+      <Component {...pageProps} />
+      <Analytics/>
+    </>
   )
 }
 
