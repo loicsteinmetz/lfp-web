499d757683b85c89a73c9d09f2107b84c9c0eaca
Loïc STEINMETZ <loic.steinmetz@pleiade.app>
2022-12-23 13:45:12 +0100

    Fix window observer

--- utils/windowDimensions.tsx
+++ utils/windowDimensions.tsx
@@ -1,15 +1,21 @@
 import { useState, useEffect } from 'react';
 
+function getWindowDimensions() {
+  if (typeof window === 'undefined') {
+    return {
+      width: 0,
+      height: 0,
+    }
+  }
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height
+  };
+}
+
 export default function useWindowDimensions() {
   const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
-
-  function getWindowDimensions() {
-    const { innerWidth: width, innerHeight: height } = window;
-    return {
-      width,
-      height
-    };
-  }
 
   useEffect(() => {
     function handleResize() {
