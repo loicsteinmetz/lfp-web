ebc58420b421927399523de10a7f2954a3ba90ad
Loïc STEINMETZ <loic.steinmetz@pleiade.app>
2023-04-08 22:34:06 +0200

    Fix labels

--- components/Label.tsx
+++ components/Label.tsx
@@ -8,7 +8,7 @@ export interface LabelProps {
   type?: 'primary' | 'grey';
 }
 
-const Text = styled.p<{type?: 'primary' | 'grey', url?: string}>`
+const Text = styled.p<{type?: 'primary' | 'grey'}>`
   display: inline-block;
   padding: 3px 7px 4px 7px;
   border-radius: 5px;
@@ -20,15 +20,15 @@ const Text = styled.p<{type?: 'primary' | 'grey', url?: string}>`
   font-weight: bold;
   
   &:hover {
-    background-color: ${({type, url}) => url ? (type === 'primary' ? Colors.GREY['0'] : Colors.GREY['500']) : 'inital'});
-    color: ${({type, url}) => url ? (type === 'primary' ? Colors.PRIMARY['500'] : Colors.GREY['0']) : 'inital'};
-    cursor: ${({url}) => url ? 'pointer' : 'inital'};
+    background-color: ${({type}) => type === 'primary' ? Colors.GREY['0'] : Colors.GREY['500']};;
+    color: ${({type}) => type === 'primary' ? Colors.PRIMARY['500'] : Colors.GREY['0']};;
+    cursor: pointer;
   }
 `
 
 const Label = ({label, url, type = 'primary'}: LabelProps) => {
   return url ? (
-    <Link href={url}><Text type={type} url={url}>{label}</Text></Link>
+    <Link href={url}><Text type={type}>{label}</Text></Link>
   ) : <Text type={type}>{label}</Text>
 }
 
